03bca2c61f3a5b0c35bd42c83d68eb085e049302
David Adams <dadams.chi@gmail.com>
2026-09-10 11:48:49 -0500

    added domain for icons

--- lib/utils/newsletter.ts
+++ lib/utils/newsletter.ts
@@ -12,17 +12,17 @@ export const EMAIL_SIGNATURE_HTML = `<div style="font-family: Arial, sans-serif;
     <tr>
       <td style="padding-right: 12px;">
         <a href="https://www.instagram.com/portergoldbergchicago" target="_blank" style="text-decoration: none;">
-          <img src="https://cdn-icons-png.flaticon.com/512/174/174855.png" alt="Instagram" width="24" height="24" style="display: block; border: 0;">
+          <img src="https://www.portergoldberg.com/images/social/instagram.png" alt="Instagram" width="24" height="24" style="display: block; border: 0;">
         </a>
       </td>
       <td style="padding-right: 12px;">
         <a href="https://www.facebook.com/PorterGoldbergResidential" target="_blank" style="text-decoration: none;">
-          <img src="https://cdn-icons-png.flaticon.com/512/733/733547.png" alt="Facebook" width="24" height="24" style="display: block; border: 0;">
+          <img src="https://www.portergoldberg.com/images/social/facebook.png" alt="Facebook" width="24" height="24" style="display: block; border: 0;">
         </a>
       </td>
       <td style="padding-right: 12px;">
         <a href="https://www.youtube.com/@PorterGoldbergResidential" target="_blank" style="text-decoration: none;">
-          <img src="https://cdn-icons-png.flaticon.com/512/174/174883.png" alt="YouTube" width="24" height="24" style="display: block; border: 0;">
+          <img src="https://www.portergoldberg.com/images/social/youtube.png" alt="YouTube" width="24" height="24" style="display: block; border: 0;">
         </a>
       </td>
     </tr>
